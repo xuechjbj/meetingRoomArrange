7fb82064c8a4ef5161b548683b038e23aef57b91
Chaojun Xue <xuechjbj88@gmail.com>
2016-01-05 10:49:39 -0800

    Initial Commit

---
+++ Algo-meeting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algo-meeting
+//
+//  Created by Chaojun Xue on 1/5/16.
+//  Copyright © 2016 Chaojun Xue. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
